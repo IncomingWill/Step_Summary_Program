5e1a17eb91937c3e472b38563f0fb50bea00df9d
IncomingWill <thewilliamedwards@gmail.com>
2022-02-01 19:54:37 -0600

    Add project files.

---
+++ Step Summary Program/Step_Summary.py
@@ -0,0 +1,7 @@
+# Title: Step Summary Program
+# Program created by William Schaeffer
+# CPS 313
+# P. 359, Exercise 12, Step Summary Program
+# 02.01.22
+
+# This program will take 5 test scores and display letter grade for each score and average score
